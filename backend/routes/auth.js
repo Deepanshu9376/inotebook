09927c82b504dd1b9ecd12c26344b14765d4e2a3
Deepanshu Thakur <Deepanshu.Thakur@Geminisolutions.com>
2024-05-15 17:33:09 +0530

    database data sving done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,13 +1,25 @@
 const express=require('express');
 const User=require("../models/User");
+const { query, validationResult} = require('express-validator');
 const router=express.Router();
 
 //Create a user using : POST "/api/auth/". doesn't require authentication"
-router.post('/',(req,res)=>{
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
-    console.log(req.body);
+router.post('/',[
+    query('name','Enter a valid name (Min 3char length)').isLength({min: 3}),
+    query('email', 'Enter a valid mail').isEmail(),
+    query('password', 'Password must be 5 char length').isLength({min: 5})
+],(req,res)=>{
+    const result = validationResult(req);
+  if (result.isEmpty()) {
+    return res.json({ errors: result.array() });
+  }
+  User.create({
+    name: req.body.name,
+    password: req.body.password,
+    email: req.body.email,
+  }).then(user=>res.json(user))
+  .catch(err=>{console.log(err)
+  res.json({err: 'Please enter a unique email', message: err.message})});
 })
 
 module.exports=router
