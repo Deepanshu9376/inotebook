58ab966f5396e970f22d005473f269d0ec2d451b
Deepanshu Thakur <Deepanshu.Thakur@Geminisolutions.com>
2024-05-16 12:32:16 +0530

    login route with jwt token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -45,7 +45,42 @@ router.post('/createUser',[
   }
   catch(error){
     console.log(error.message)
-    res.status(500).send("Some error occured");
+    res.status(500).send("Internal Server Error");
+  }
+})
+
+
+//Authenticate a user using : POST "/api/auth/login". No login required"
+router.post('/login',[
+    query('email', 'Enter a valid mail').isEmail(),
+    query('password', 'Password must be 5 char length').isLength({min: 5})
+], async(req,res)=>{
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const {email,password}=req.body;
+  try{
+    let user= await User.findOne({email});
+    if(!user){
+      return res.status(400).json({error:"please try with correct credentials"});
+    }
+    const passwordCompare=await bcrypt.compare(password,user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error:"please try with correct credentials"});
+    }
+    const data={
+      user:{
+        id: user.id
+      }
+    }
+    const authToken=jwt.sign(data,JWT_SECRET);
+    res.json({authToken})
+  }
+  catch(error){
+    console.log(error.message)
+    res.status(500).send("Internal Server Error");
   }
 })
 
